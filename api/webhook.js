419842f07516014e6eeec3dbaed59efed707fe7d
wizanesat <wizanesat@gmail.com>
2026-04-20 19:03:16 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,1 +1,13 @@
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        const event = req.body;
+        console.log("وصلت إشارة دفع جديدة:", event);
 
+        // هنا يمكنك تحديث حالة الطلب في Firebase إلى "completed"
+        // بناءً على id الطلب القادم من Chargily
+
+        res.status(200).send('Webhook Received');
+    } else {
+        res.status(405).send('Method Not Allowed');
+    }
+}
